f0edad5adeccac551a5dcd2a8325d3eb5f90924b
HalabSaudi <ashrafq952@gmail.com>
2025-12-11 15:57:29 +0300

    / done

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const vendorRoutes = require("./src/hbs/routes/venderAccountRoute");
 // ----------------- MIDDLEWARES -----------------
 app.use(
   cors({
-    origin: [ "http://localhost:5173",  "http://127.0.0.1:5173", "https://al-wessilholding.com", "https://halab-saudi.vercel.app/"],
+    origin: [ "http://localhost:5173",  "http://127.0.0.1:5173", "https://al-wessilholding.com", "https://halab-saudi.vercel.app" ],
     credentials: true,
   })
 );
